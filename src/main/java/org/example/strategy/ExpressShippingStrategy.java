72444a69c21785cf5bee0c1883ff6e1eca46cf1f
2431380yasamingol <yasamingol>
2023-11-22 08:56:04 -0800

    Implement ExpressShippingStrategy class.

--- src/main/java/org/example/strategy/ExpressShippingStrategy.java
+++ src/main/java/org/example/strategy/ExpressShippingStrategy.java
@@ -1,2 +1,8 @@
+package org.example.strategy;
+
 public class ExpressShippingStrategy implements ShippingStrategy {
+    @Override
+    public void ship() {
+        System.out.println("Package is shipped by Express Shipping - Shipping Cost: 30$");
+    }
 }
