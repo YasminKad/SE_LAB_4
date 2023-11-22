e0ac132932301d2a9c918db4b9a9cd2004ed037e
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2023-11-22 21:07:50 +0330

    Implement ShippingSystemTest.java tests

--- src/test/java/ShippingSystemTest.java
+++ src/test/java/ShippingSystemTest.java
@@ -1,2 +1,35 @@
+import org.example.strategy.ExpressShippingStrategy;
+import org.example.strategy.StandardShippingStrategy;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShippingSystemTest {
+    @Test
+    void shipFunctionShouldPrintExpressShippingMessage() {
+        ExpressShippingStrategy strategy = new ExpressShippingStrategy();
+        assertEquals("Package is shipped by Express Shipping.", captureOutput(strategy::ship));
+    }
+
+    @Test
+    void shipShouldPrintStandardShippingMessage() {
+        StandardShippingStrategy strategy = new StandardShippingStrategy();
+        assertEquals("Package is shipped by Standard Shipping.", captureOutput(strategy::ship));
+    }
+
+    private String captureOutput(Runnable code) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        try {
+            code.run();
+            return outputStream.toString().trim();
+        } finally {
+            System.setOut(originalOut);
+        }
+    }
 }
