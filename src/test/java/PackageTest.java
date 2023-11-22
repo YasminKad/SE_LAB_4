58549a011a6a50760706aa79be37f64354eabf45
2431380yasamingol <yasamingol>
2023-11-22 09:13:45 -0800

    Write Package test.
    We have written the needed tests for this class with the approach of TDD before developing the main class functionalities.

--- src/test/java/PackageTest.java
+++ src/test/java/PackageTest.java
@@ -1,3 +1,47 @@
+import mocks.PackageStateMock;
+import mocks.ShippingStrategyMock;
+import org.example.state.InTransitState;
+import org.example.state.PackageState;
+import org.example.strategy.ShippingStrategy;
+import org.example.strategy.StandardShippingStrategy;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.example.Package;
 
 class PackageTest {
+
+    @Test
+    void setShippingStrategy_shouldSetStrategy() {
+        Package myPackage = new Package();
+        ShippingStrategy strategy = new StandardShippingStrategy();
+        myPackage.setShippingStrategy(strategy);
+        assertEquals(strategy, myPackage.getShippingStrategy());
+    }
+
+    @Test
+    void setPackageState_shouldSetState() {
+        Package myPackage = new Package();
+        PackageState state = new InTransitState(myPackage);
+        myPackage.setPackageState(state);
+        assertEquals(state, myPackage.getPackageState());
+    }
+
+    @Test
+    void shipPackage_shouldInvokeShippingStrategy() {
+        Package myPackage = new Package();
+        ShippingStrategyMock strategyMock = new ShippingStrategyMock();
+        myPackage.setShippingStrategy(strategyMock);
+        myPackage.shipPackage();
+        assertTrue(strategyMock.isShipMethodInvoked());
+    }
+
+    @Test
+    void updatePackageState_shouldInvokeUpdateState() {
+        Package myPackage = new Package();
+        PackageStateMock stateMock = new PackageStateMock(myPackage);
+        myPackage.setPackageState(stateMock);
+        myPackage.updatePackageState();
+        assertTrue(stateMock.isUpdateStateMethodInvoked());
+    }
 }
