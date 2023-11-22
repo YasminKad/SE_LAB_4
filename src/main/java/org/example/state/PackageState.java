16232277c5578c67367734de202069a69818daeb
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2023-11-22 20:52:02 +0330

    Implement PackageState.java class

--- src/main/java/org/example/state/PackageState.java
+++ src/main/java/org/example/state/PackageState.java
@@ -1,2 +1,13 @@
+package org.example.state;
+
+import org.example.Package;
+
 public class PackageState {
+    private Package userPackage;
+    public PackageState(Package userPackage) {
+        this.userPackage = userPackage;
+    }
+    public void updateState(){
+    }
+
 }
