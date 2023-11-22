dc44f16f3c04cd1288556b4f76ec478b8ce67656
2431380yasamingol <yasamingol>
2023-11-22 08:50:16 -0800

    Write tests for different strategies.
    We have first designed tests to develop our program with a TDD approach.

--- src/test/java/Strategy/ExpressShippingStrategyTest.java
+++ src/test/java/Strategy/ExpressShippingStrategyTest.java
@@ -1,2 +1,29 @@
+package Strategy;
+import org.example.strategy.StandardShippingStrategy;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ExpressShippingStrategyTest {
+    @Test
+    void shipFunctionShouldPrintStandardShippingMessage() {
+        StandardShippingStrategy strategy = new StandardShippingStrategy();
+        assertEquals("Package is shipped by Standard Shipping - Shipping Cost: 15$", captureOutput(strategy::ship));
+    }
+
+    private String captureOutput(Runnable code) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        try {
+            code.run();
+            return outputStream.toString().trim();
+        } finally {
+            System.setOut(originalOut);
+        }
+    }
 }
--- src/test/java/Strategy/StandardShippingStrategyTest.java
+++ src/test/java/Strategy/StandardShippingStrategyTest.java
@@ -1,2 +1,28 @@
+package Strategy;
+import org.example.strategy.ExpressShippingStrategy;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StandardShippingStrategyTest {
+    @Test
+    void shipShouldPrintExpressShippingMessage() {
+        ExpressShippingStrategy strategy = new ExpressShippingStrategy();
+        assertEquals("Package is shipped by Express Shipping - Shipping Cost: 30$", captureOutput(strategy::ship));
+    }
+    private String captureOutput(Runnable code) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        try {
+            code.run();
+            return outputStream.toString().trim();
+        } finally {
+            System.setOut(originalOut);
+        }
+    }
 }
