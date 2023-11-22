4d905e466e5c9386f4f6c6093a7359309f85713d
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2023-11-22 20:45:45 +0330

    Implement InTransitState.java class

--- src/main/java/org/example/state/InTransitState.java
+++ src/main/java/org/example/state/InTransitState.java
@@ -1,3 +1,12 @@
+package org.example.state;
+
+import org.example.Package;
 
 public class InTransitState extends PackageState {
+    public InTransitState(Package userPackage) {
+        super(userPackage);
+    }
+    public void updateState() {
+        System.out.println("Package is now In Transit.");
+    }
 }
